975545f1b6bde7cce26adec78ed97864e7a60b4d
Bhanu-12 <Shrivastava1219@gmail.com>
2019-07-02 21:49:18 +0530

    second commit

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const publicPath = path.join(__dirname, "..", "public");
+var server = require("http").createServer(app);
 const port = process.env.PORT || 8000;
 app.use(express.static(publicPath));
 
@@ -9,7 +10,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(publicPath);
   console.log("Server is up!");
 });
